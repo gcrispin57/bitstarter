b3b0e4b7807b10d22d0f424f4ac8b27723d337a2
Gordon Crispin <gcrispin1@gmail.com>
2013-07-12 05:06:45 +0000

    Changed display to Hello World from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf =  new Buffer(ReadFileSync('index.html'));
+buf =  buf_to_string();
+console.log(buf);
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
