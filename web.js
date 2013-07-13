f71a84aca3c2666c59c1b393f0e13434ba4ba784
Gordon Crispin <gcrispin1@gmail.com>
2013-07-13 10:23:10 +0000

    sat 6:21

--- web.js
+++ web.js
@@ -5,6 +5,10 @@ var app = express.createServer(express.logger());
 // var buf =  new Buffer(fs.readFileSync('index.html'));
 // str =  buf.toString();
 
+buf = new Buffer(256);
+len = buf.write('\u00bd + \u00bc = \u00be', 0);
+console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+
 app.get('/', function(request, response) {
   response.send('helloooooo wirth');
 });
