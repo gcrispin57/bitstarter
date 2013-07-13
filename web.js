d85d2e7ccb4bf64a49011987271d4696ddc520af
Gordon Crispin <gcrispin1@gmail.com>
2013-07-13 11:01:06 +0000

    sat 6:59

--- web.js
+++ web.js
@@ -2,6 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+var data;
+data = (fs.readFileSync('index.html', encoding String);
+console.log(data)
+// console.log(fs.readFileSync('index.html', encoding String));
+// console.log(len + " bytes: " + buf1.toString('utf8', 0, len));
+
 // var buf =  new Buffer(fs.readFileSync('index.html'));
 // str =  buf.toString();
 
@@ -10,7 +17,7 @@ len = buf.write('\u00bd + \u00bc = \u00be', 0);
 console.log(len + " bytes: " + buf.toString('utf8', 0, len));
 
 app.get('/', function(request, response) {
-  response.send('helloooooo wirth');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
