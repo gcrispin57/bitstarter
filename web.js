3673cd0b3c28c29fed3646db8b69b7fe8d69499d
Gordon Crispin <gcrispin1@gmail.com>
2013-07-13 11:20:09 +0000

    sat 7:18

--- web.js
+++ web.js
@@ -2,22 +2,24 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var text = fs.readFileSync('index.html','utf8');
+//console.log(text);
 
-var data;
-data = (fs.readFileSync('index.html', encoding String);
-console.log(data)
+// var data;
+//data = (fs.readFileSync('index.html', encoding String);
+//console.log(data)
 // console.log(fs.readFileSync('index.html', encoding String));
 // console.log(len + " bytes: " + buf1.toString('utf8', 0, len));
 
 // var buf =  new Buffer(fs.readFileSync('index.html'));
 // str =  buf.toString();
 
-buf = new Buffer(256);
-len = buf.write('\u00bd + \u00bc = \u00be', 0);
-console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+//buf = new Buffer(256);
+//len = buf.write('\u00bd + \u00bc = \u00be', 0);
+//console.log(len + " bytes: " + buf.toString('utf8', 0, len));
 
 app.get('/', function(request, response) {
-  response.send(data);
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
