176e1f4bf0bce178e34c4e3fa3a5869fbd7f31a9
Gordon Crispin <gcrispin1@gmail.com>
2013-07-14 01:08:09 +0000

    read and display from index.html

--- web.js
+++ web.js
@@ -2,12 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//var buf = new Buffer(256);
 var fs = require('fs');
-//var text = fs.readFileSync('index.html','utf8');
-//var len = buf.write(text, "utf-8");
-//var str = buf.toString("utf-8",0, len);
-//console.log(len + " bytes: " + str);
 
 app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html', 'utf8'));
